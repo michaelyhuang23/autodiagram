01b4124fd28ac78620899d1b7928c8798ca84602
Michael Huang <michaelyhuang23@gmail.com>
2023-11-05 14:14:28 -0500

    hide .env

--- data_generation/tikz_gen_gpt4.py
+++ data_generation/tikz_gen_gpt4.py
@@ -2,7 +2,7 @@ from gpt_bot import GPTBot
 import pandas as pd
 
 ideaBotInstruction = "You are an AI assistant that helps researchers train another GPT model by generating good prompts. You are to help researchers generate creative prompts for various kinds of TikZ diagrams that students typically see in math and physics classes. Your prompts will be used by another GPT model to generate TikZ codes. Generate plain text. Do not enclose the prompt you generate in quotation marks. The prompts you generate should be sufficiently simple that a GPT model can easily generate the accurating TikZ code corresponding to it. And you should focus on generating geometric diagrams or physics diagrams like free body diagrams."
-ideaBotPrompt = "Generate one prompt for producing a creative TikZ diagram that depicts a free body diagram one might see in a physics textbook."
+ideaBotPrompt = "Generate one prompt for producing a creative TikZ diagram that depicts a free body diagram one might see in a physics textbook. Do not prompt the AI to produce labels or Latin characters."
 
 ideaBot = GPTBot(ideaBotInstruction, auto_regressive=False)
 print(ideaBot.gen_completion(ideaBotPrompt))
@@ -13,9 +13,12 @@ tikzBot = GPTBot(tikzBotInstruction, auto_regressive=False)
 df = pd.DataFrame(columns=['idea', 'response'])
 ideas = []
 tikzcodes = []
-for i in range(10):
+for i in range(100):
     idea = ideaBot.gen_completion(ideaBotPrompt)
     completion = tikzBot.gen_completion(idea)
+    startIdx = completion.find('\documentclass')
+    endIdx = completion.find('\end{document}') + len('\end{document}')
+    completion = completion[startIdx:endIdx]
     ideas.append(idea)
     tikzcodes.append(completion)
 
