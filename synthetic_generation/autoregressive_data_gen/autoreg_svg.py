521514337412d6964f6f2bd81ef673bd253308c5
Michael Huang <michaelyhuang23@gmail.com>
2023-12-06 23:45:51 -0500

    modify data gen

--- synthetic_generation/autoregressive_data_gen/autoreg_svg.py
+++ synthetic_generation/autoregressive_data_gen/autoreg_svg.py
@@ -24,17 +24,17 @@ from augmentations import curve_line
 def draw_line(image, start_point, end_point, line_color=(0, 0, 0), line_thickness=2):
     cv2.line(image, start_point, end_point, line_color, line_thickness)
 
-def sort_point_pairs_by_distance(point_pairs):
-    def distance(pair):
+def sort_point_pairs_by_pair(point_pairs):
+    def pair_key(pair):
         (x0, y0), (x1, y1) = pair
-        return math.sqrt((x0 - x1)**2 + (y0 - y1)**2)
+        return min(y0, y1)*10000 + min(x0, x1) 
 
-    return sorted(point_pairs, key=distance)
+    return reversed(sorted(point_pairs, key=pair_key))
 
-def generate_points(num_lines, num_rect=0, num_tri=0):
+def generate_points(num_objs):
     points = []
 
-    num_rectangles = random.randint(0,2)
+    num_rectangles = random.randint(0,num_objs)
     for j in range(num_rectangles):
         start_point = (random.randint(5, width-5), random.randint(5, height-5))
         end_point = (random.randint(5, width-5), random.randint(5, height-5))
@@ -46,7 +46,7 @@ def generate_points(num_lines, num_rect=0, num_tri=0):
         points.append([end_point, projection1])
         points.append([end_point, projection2])
 
-    num_triangles = random.randint(0,1)
+    num_triangles = random.randint(0,num_objs)
     for j in range(num_triangles):
         pointA = (random.randint(0, width-1), random.randint(0, height-1))
         pointB = (random.randint(0, width-1), random.randint(0, height-1))
@@ -56,7 +56,7 @@ def generate_points(num_lines, num_rect=0, num_tri=0):
         points.append([pointA, pointC])
         points.append([pointB, pointC])
 
-    num_single_line = max(num_lines - 2*num_rectangles - num_triangles,0)
+    num_single_line = random.randint(0,num_objs)
     for j in range(num_single_line):
         # Draw a line by defining the two endpoints. 
         start_point = (random.randint(0, width-1), random.randint(0, height-1))
@@ -64,12 +64,8 @@ def generate_points(num_lines, num_rect=0, num_tri=0):
         points.append([start_point, end_point])
     
     # Sort the lines based on length:
-    points = sort_point_pairs_by_distance(points)
+    points = sort_point_pairs_by_pair(points)
     num_actual_lines = 4 * num_rectangles + 3 * num_triangles + num_single_line  
-
-    # print(f"rect,tri,single: {num_rectangles}, {num_triangles}, {num_single_line}")
-    # print(len(points))
-    # print(num_actual_lines)
 
     return points, num_actual_lines
 
@@ -77,42 +73,37 @@ def generate_points(num_lines, num_rect=0, num_tri=0):
 # Image dimensions and line parameters
 width, height = 672, 896
 
-with open("line_coords.pickle", "wb") as handle:
-    num_data_points = 10000
-    for i in range(num_data_points):
+num_data_points = 10000
+all_points = []
+for i in range(num_data_points):
 
-        if (i % 25 == 0):
-            print(i)
+    if (i % 25 == 0):
+        print(i)
 
-        #Generate the points
-        num_lines_to_draw = random.randint(1,5)
-        points,num_lines = generate_points(num_lines_to_draw)
+    #Generate the points
+    points,num_lines = generate_points(5)
 
-        # Create a blank image
-        image = np.ones((height, width), np.float32)
+    # Create a blank image
+    image = np.ones((height, width), np.float32)
+    # Iteratively add the necessary lines
+    for j in range(num_lines):
+        line_thickness = 5
 
-        # Iteratively add the necessary lines
-        for j in range(num_lines):
-            line_thickness = 5
+        # Call data augmentation function which returns curved line
+        additive_img, variance_width = curve_line(points[j][0], points[j][1], line_thickness, (height, width), amp=0.01)
+        points[j].append(variance_width)
 
-            # Call data augmentation function which returns curved line
-            additive_img, variance_width = curve_line(points[j][0], points[j][1], line_thickness, (height, width), amp=0.01)
-            variance_width = round(variance_width,1)
-            points[j].append(variance_width)
+        # draw_line(image, start_point, end_point, line_color, line_thickness)
+        image = np.clip(image * additive_img,0,1)
 
-            # draw_line(image, start_point, end_point, line_color, line_thickness)
-            image = np.clip(image * additive_img,0,1)
+        # Save the image
+        output_filename = f'imgfiles/{i}-{j}.jpg'
+        cv2.imwrite(output_filename, np.round(image*255))
 
-            # Save the image
-            output_filename = f'imgfiles/line_drawing_test_{i}-{j}.jpg'
-            cv2.imwrite(output_filename, np.round(image*255))
-        
-        # Write the idealized data onto the text file
-        for point in reversed(points):
-            # f.write(str(point))
-            pickle.dump(point, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        # f.write("\n")
+    all_points.append(points)
 
+with open("line_coords.pickle", "wb") as handle: 
+    pickle.dump(all_points, handle)
 #todo: 
 # - (done) make the lines increase in size
 # - (done) figure out how to call michael's data aug
