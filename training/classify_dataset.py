60a09fddd2399e81c28e52072322873f94450a5f
Michael Huang <michaelyhuang23@gmail.com>
2023-12-07 04:05:05 -0500

    cloud ready

--- training/classify_dataset.py
+++ training/classify_dataset.py
@@ -6,19 +6,50 @@ import os
 import io
 from PIL import Image
 import pickle
+import resource
+resource.setrlimit(resource.RLIMIT_NOFILE, (1000000,-1))
 
 class ClassifyDataset(Dataset):
     def __init__(self, root_dir):
         super().__init__()
-        self.source_images_num = 0
-        self.labels = self.read_labels(os.path.join(root_dir, "line_coords.pickle"))
-        self.images = self.load_images(os.path.join(root_dir, "imgfiles"))
+        self.read_data(root_dir)
 
-    def read_labels(self, label_file):
-        labels = []
-        with open(label_file, "rb") as openfile:
-            labels = pickle.load(openfile)
-        labels = [torch.tensor([label[2], *label[0], *label[1]]) for label in labels]
+    def read_data(self, root_dir):
+        self.labels = []
+        self.images = []
+        img_dir = os.path.join(root_dir, "imgfiles")
+        labels_dir = os.path.join(root_dir, "labels")
+        img_seconds = {}
+        for img_file in os.listdir(img_dir):
+            if '.jpg' not in img_file: continue
+            first = int(img_file.split('-')[0])
+            second = int(img_file.split('-')[1].split('.')[0])
+            if first not in img_seconds:
+                img_seconds[first] = second
+            else:
+                img_seconds[first] = max(img_seconds[first], second)
+        label_ids = set([int(label_file.split('.')[0]) for label_file in os.listdir(labels_dir) if '.pkl' in label_file])
+        label_ids = label_ids.intersection(img_seconds.keys())
+
+        for label_id in label_ids:
+            label_path = os.path.join(labels_dir, f'{label_id}.pkl')
+            with open(label_path, "rb") as openfile:
+                labels = pickle.load(openfile)
+            labels = [torch.tensor([label[2], *label[0], *label[1]]) for label in labels]
+            self.labels += labels
+            for j in range(img_seconds[label_id]+1):
+                file_path = os.path.join(img_dir, f"{label_id}-{j}.jpg")
+                image = Image.open(file_path)
+                self.images.append(torchvision.transforms.functional.to_tensor(image).float())
+
+    def read_labels(self, label_folder):
+        for label_file in os.listdir(label_folder):
+            if '.pkl' not in label_file: continue
+            label_path = os.path.join(label_folder, label_file)
+            with open(label_path, "rb") as openfile:
+                labels = pickle.load(openfile)
+            labels = [torch.tensor([label[2], *label[0], *label[1]]) for label in labels]
+            
         return torch.stack(labels)
 
     def parse_labels(self, label_file):
@@ -49,9 +80,9 @@ class ClassifyDataset(Dataset):
         return len(self.images)
     
     def __getitem__(self, index):
-        print("index:", index)
         image = self.images[int(index)]
         label = self.labels[int(index)]
         return image, label
 
-dataset = ClassifyDataset(root_dir='../data/classification_dataset')
+
+dataset = ClassifyDataset(root_dir='../data/detection_dataset')
